87d9616d09a640e3dd96e7f0664788c083c99693
weeveedenny <dennisodalonu@gmail.com>
2021-11-19 21:58:04 +0100

    Intro

---
+++ app/src/main/java/com/olamachia/weeksixtaskandroidsq009/AddContacts.kt
@@ -0,0 +1,11 @@
+package com.olamachia.weeksixtaskandroidsq009
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddContacts : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_contacts)
+    }
+}
